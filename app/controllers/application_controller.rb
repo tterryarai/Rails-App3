8af869906cb0114d99e90edc1ecc42b25ef7f679
Toshimasa Arai <tterry@tc4.so-net.ne.jp>
2019-08-25 11:20:18 +0900

    for mobile

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,18 @@
 class ApplicationController < ActionController::Base
 
   before_action :authenticate_user!
+  before_action :detect_user_agent
+
+  private
+  def detect_user_agent
+    ua = request.env["HTTP_USER_AGENT"]
+    if ua.include?('Mobile') || ua.include?('Android')
+      @ua = 'Mobile'
+    else
+      # debug purpose
+      @ua = 'Mobile'
+      #@ua = 'Desktop'
+    end
+  end
 
 end
